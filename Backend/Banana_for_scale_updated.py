7ddbfc5a656d07f65b512894dcd7b6c265653120
wkeu <matt.slevin@ucdconnect.ie>
2016-12-21 14:01:09 +0000

    Refactoring
    Created method extract_contors_and_imort_image, which was refactored
    from main
    
    Refactored all colour values as global contants.

--- Backend/Banana_for_scale_updated.py
+++ Backend/Banana_for_scale_updated.py
@@ -3,13 +3,27 @@ from scipy.spatial import distance as dist
 import cv2
 import imutils
 
+#Global Definitions
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+PINK = (255, 0, 255)
+WHITE = (255, 255, 255)
+Contour_threshold_Area = 200
+
+
 class Image:
+
+  def __init__(self):
+      self.test="Test"
+
+  #TODO:Not clear the difference between x_cord and x_cordinate
   def midpoint(self,x_cord, y_cord):
         x_cordinate = (x_cord[0] + y_cord[0]) * 0.5
         y_cordinate = (x_cord[1] + y_cord[1]) * 0.5
-        # return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
         return (x_cordinate, y_cordinate)
 
+  #TODO:Comment on how it works
   def get_pixel_per_unit(self,cntores, ref_width):
     for c in cntores:
         box = self.get_bounding_box(c)
@@ -26,6 +40,7 @@ class Image:
     box = cv2.minAreaRect(c)
     print("box calling ", box)
 
+    #TODO:Is this nessary
     if imutils.is_cv2():
         box = cv2.cv.BoxPoints(box)
     else:
@@ -46,6 +61,7 @@ class Image:
     (X_top_right_bottom_right, Y_top_right_bottom_right) = self.midpoint(top_right_point, bottom_right_point)
 
     # Computing midpoint between the top-right and bottom-right
+    #TODO:Repditive code
     distX = dist.euclidean((X_top_left_top_right, Y_top_left_top_right),
                            (X_bottom_left_bottom_right, Y_bottom_left_bottom_right))
     distY = dist.euclidean((X_top_left_bottom_left, Y_top_left_bottom_left),
@@ -57,34 +73,33 @@ class Image:
 
   def test_method(self,message):
       print("Test message"+message)
+      print(self.test)
+
+  def extract_contors_and_import_image(self, fname_image):
+      # read the image
+      # image = cv2.imread(args["image"])
+      image = cv2.imread(fname_image)
+      # convert the image to gray scale, and blur it using GaussianBlur
+      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      gray = cv2.GaussianBlur(gray, (7, 7), 0)
+      # perform edge detection,
+      detected_edges = cv2.Canny(gray, 50, 100)
+      # perform a dilation
+      detected_edges = cv2.dilate(detected_edges, None, iterations=1)
+      # perform erosion to close gaps in between object edges
+      detected_edges = cv2.erode(detected_edges, None, iterations=1)
+      # find contours in the edge map
+      cntores = cv2.findContours(detected_edges.copy(), cv2.RETR_EXTERNAL,
+                                 cv2.CHAIN_APPROX_SIMPLE)
+      return cntores, image
 
   def main(self):
-    #TODO: Comment is not sufficient, arguments not suffiecient
-    #TODO: No Magic Numbers
 
     # For testing image is in directory
     fname_image = "Image_refrence_4.jpg"
     ref_width = 2.9
 
-    # read the image
-    # image = cv2.imread(args["image"])
-    image = cv2.imread(fname_image)
-
-    # convert the image to gray scale, and blur it using GaussianBlur
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-    # perform edge detection,
-    detected_edges = cv2.Canny(gray, 50, 100)
-    # perform a dilation
-    detected_edges = cv2.dilate(detected_edges, None, iterations=1)
-
-    # perform erosion to close gaps in between object edges
-    detected_edges = cv2.erode(detected_edges, None, iterations=1)
-
-    # find contours in the edge map
-    cntores = cv2.findContours(detected_edges.copy(), cv2.RETR_EXTERNAL,
-                               cv2.CHAIN_APPROX_SIMPLE)
+    cntores, image = self.extract_contors_and_import_image(fname_image)
     cntores = cntores[0] if imutils.is_cv2() else cntores[1]
 
     pixelsPerUnit = self.get_pixel_per_unit(cntores, ref_width)
@@ -92,19 +107,21 @@ class Image:
 
     for c in cntores:
         # if the contour is not sufficiently large, ignore it
-        if cv2.contourArea(c) < 200: 
+        if cv2.contourArea(c) < Contour_threshold_Area:
             continue
 
         box = self.get_bounding_box(c)
         print("box in main ", box)
         orig = image.copy()
-        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+        cv2.drawContours(orig, [box.astype("int")], -1, GREEN, 2)
 
         # iterate over the original points to draw circles
         for (x_points, y_points) in box:
-            cv2.circle(orig, (int(x_points), int(y_points)), 5, (0, 0, 255), -1)
+            cv2.circle(orig, (int(x_points), int(y_points)), 5, BLUE, -1)
+
 
         points = self.get_distance_cordinate_points(box)
+        # TODO: Bad Smell Code is repiitive and unreeadible
         distX, distY = points[0][0], points[0][1]
         (X_top_left_top_right, Y_top_left_top_right) = points[1][0]
         (X_bottom_left_bottom_right, Y_bottom_left_bottom_right) = points[1][1]
@@ -112,33 +129,35 @@ class Image:
         (X_top_right_bottom_right, Y_top_right_bottom_right) = points[1][3]
 
         # draw the midpoints on the image
-        #TODO: Bad Smell, repeditive code
-        cv2.circle(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)), 5, (255, 0, 0), -1)
+        #TODO: Bad Smell, repeditive code, Should be created into its own object
+        #TODO: Replace numbers 5 and -1 with something more meaningfull
+        cv2.circle(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)), 5, RED, -1)
+        cv2.circle(orig, (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)), 5, RED, -1)
+        cv2.circle(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)), 5, RED, -1)
+        cv2.circle(orig, (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)), 5, RED, -1)
 
         # draw lines between the midpoints
         cv2.line(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)),
                  (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)),
-                 (255, 0, 255), 2)
+                 PINK, 2)
         cv2.line(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)),
                  (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)),
-                 (255, 0, 255), 2)
+                 PINK, 2)
 
         # Drawing the objects according to their size on the image
         cv2.putText(orig, "{:.1f}in".format(distX / pixelsPerUnit),
                     (int(X_top_left_top_right - 15), int(Y_top_left_top_right - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
-                    (255, 255, 255), 2)
+                    WHITE, 2)
         cv2.putText(orig, "{:.1f}in".format(distY / pixelsPerUnit),
                     (int(X_top_right_bottom_right + 10), int(Y_top_right_bottom_right)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
-                    (255, 255, 255), 2)
+                    WHITE, 2)
 
         # show the generated image
         cv2.imshow("Image", orig)
         cv2.waitKey(0)
 
-#Test of the code 
+#Test of the code
 test_image=Image()
 test_image.test_method("12345678")
 test_image.main()
+
