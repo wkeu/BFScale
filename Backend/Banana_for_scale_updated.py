acce9b15831ccb5df7a91110de16922510044406
wkeu <matt.slevin@ucdconnect.ie>
2016-12-22 13:15:04 +0000

    Refactoring: Back up before creating class measurement
    List of Changes:
    -Created class varible ref_object_index
    -Created method to update this class varible
    -Created print statments for debugging purposes
    -Extracted methods:
    -Print dimensions on object
    -Draw centre line
    -Draw points on corners
    -Updated ref_width to be an image class varible
    -Update of naming for cntores as:
    -differed from traditional naming convention
    -if statment containing imutils.is_cv2() was redundant
    -cnt or cntores[1] is a list of all the contors within the image
    -Added counter x into main loop for debugging purposes
    -Created class measurment placeholder

--- Backend/Banana_for_scale_updated.py
+++ Backend/Banana_for_scale_updated.py
@@ -11,11 +11,11 @@ PINK = (255, 0, 255)
 WHITE = (255, 255, 255)
 Contour_threshold_Area = 200
 
-
 class Image:
 
   #Class Varibles
   ref_width = 2.9
+  ref_object_index= 0 #measurment
 
   #Class Constructor
   def __init__(self,fname_image):
@@ -29,6 +29,9 @@ class Image:
         y_cordinate = (x_cord[1] + y_cord[1]) * 0.5
         return (x_cordinate, y_cordinate)
 
+  def update_ref_object(self,new_ref_index):
+      self.ref_object_index=new_ref_index
+
   #TODO:Comment on how it works
   def get_pixel_per_unit(self,ref_object, ref_width):
         box = self.get_bounding_box(ref_object)
@@ -36,6 +39,8 @@ class Image:
         distX, distY = points[0][0], points[0][1] #TODO: Bad Smell distX never used
         pixelsPerUnit = distY / ref_width
         dimensionY = distY / pixelsPerUnit
+        print("sucessful excution of get_pixel_per_unit")
+        print("ref object is :" + str(ref_object))
         if dimensionY == ref_width:
           print("dimensionY == ref_width")
         return pixelsPerUnit
@@ -44,7 +49,7 @@ class Image:
   def get_bounding_box(self, c):
     box = cv2.minAreaRect(c)
     print("box calling ", box)
-
+   # print("c is :" + str(c)) ATM contors array being passed in here 
     #TODO:Is this nessary
     if imutils.is_cv2():
         box = cv2.cv.BoxPoints(box)
@@ -53,6 +58,7 @@ class Image:
 
     box = np.array(box, dtype="int")
     box = imutils.perspective.order_points(box)
+    print("sucessful excution of get_bounding_box")
     return box
 
 
@@ -67,6 +73,7 @@ class Image:
 
     # Computing midpoint between the top-right and bottom-right
     #TODO:Repditive code
+    print("sucessful excution of get_distance_cordinate_points")
     distX = dist.euclidean((X_top_left_top_right, Y_top_left_top_right),
                            (X_bottom_left_bottom_right, Y_bottom_left_bottom_right))
     distY = dist.euclidean((X_top_left_bottom_left, Y_top_left_bottom_left),
@@ -92,39 +99,79 @@ class Image:
       # find contours in the edge map
       cntores = cv2.findContours(detected_edges.copy(), cv2.RETR_EXTERNAL,
                                  cv2.CHAIN_APPROX_SIMPLE)
+      print("sucessful excution of extract_contors_and_import_image")
       return cntores, image
+
+  #TODO: Xtop_left... can be extracted so that we are only inserting (shape,orig,ppu)
+  #TODO: Add colour as an input parameter
+  #TODO:
+  def print_dimensions_on_object(self, X_top_left_top_right, X_top_right_bottom_right, Y_top_left_top_right,
+                                 Y_top_right_bottom_right, distX, distY, orig, pixelsPerUnit):
+      # Drawing the objects according to their size on the image
+      cv2.putText(orig, "{:.1f}in".format(distX / pixelsPerUnit),
+                  (int(X_top_left_top_right - 15), int(Y_top_left_top_right - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
+                  WHITE, 2)
+      cv2.putText(orig, "{:.1f}in".format(distY / pixelsPerUnit),
+                  (int(X_top_right_bottom_right + 10), int(Y_top_right_bottom_right)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
+                  WHITE, 2)
+
+  def draw_centre_line(self, X_bottom_left_bottom_right, X_top_left_bottom_left, X_top_left_top_right,
+                       X_top_right_bottom_right, Y_bottom_left_bottom_right, Y_top_left_bottom_left,
+                       Y_top_left_top_right, Y_top_right_bottom_right, orig):
+      cv2.line(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)),
+               (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)),
+               PINK, 2)
+      cv2.line(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)),
+               (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)),
+               PINK, 2)
+
+  def draw_points_on_corners(self, X_bottom_left_bottom_right, X_top_left_bottom_left, X_top_left_top_right,
+                             X_top_right_bottom_right, Y_bottom_left_bottom_right, Y_top_left_bottom_left,
+                             Y_top_left_top_right, Y_top_right_bottom_right, orig):
+      # draw the midpoints on the image
+      # TODO: Bad Smell, repeditive code, Should be created into its own object
+      # TODO: Replace numbers 5 and -1 with something more meaningfull
+      cv2.circle(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)), 5, RED, -1)
+      cv2.circle(orig, (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)), 5, RED, -1)
+      cv2.circle(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)), 5, RED, -1)
+      cv2.circle(orig, (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)), 5, RED, -1)
 
   def main(self):
 
-    # For testing image is in directory
-    fname_image = "Image_refrence_4.jpg"
-    ref_width = 2.9
-
-    cntores, image = self.extract_contors_and_import_image(fname_image)
+    #TODO:Bad smell no need to pass self.fname_image
+    #Import image and extract contors
+    #TODO:Cntores should be a class varible as should image
+    cntores, image = self.extract_contors_and_import_image(self.fname_image)
     
     # TODO: comment on whats happening here, the if statment is never used
-    #Wil not work without this line
-    cntores = cntores[0] if imutils.is_cv2() else cntores[1]
-    ref_object=cntores[0]
+    # TODO:
+    #Wil not work without this line, dont know why
+    cnt = cntores[1] #Extract Each object
+    ref_object=cnt[self.ref_object_index] #Extract this magic number so it is ref_object index
     
-    #TODO:
-    pixelsPerUnit = self.get_pixel_per_unit(ref_object, ref_width)
+    #TODO:Bad smell no need to pass self.ref_width
+    pixelsPerUnit = self.get_pixel_per_unit(ref_object, self.ref_width)
     print("calculated unit pixels ", pixelsPerUnit)
 
-    for c in cntores:
+    x=0
+    
+    for c in cnt:
         # if the contour is not sufficiently large, ignore it
+        print(str(x))        
+        x+=1
         if cv2.contourArea(c) < Contour_threshold_Area:
             continue
 
         box = self.get_bounding_box(c)
         print("box in main ", box)
-        orig = image.copy()
-        cv2.drawContours(orig, [box.astype("int")], -1, GREEN, 2)
+        orig = image.copy()  #Copy the image so that we keep the orginal image intact
+        cv2.drawContours(orig, [box.astype("int")], -1, GREEN, 2) #Draw green box
 
+        """
         # iterate over the original points to draw circles
         for (x_points, y_points) in box:
             cv2.circle(orig, (int(x_points), int(y_points)), 5, BLUE, -1)
-
+        """
 
         points = self.get_distance_cordinate_points(box)
         # TODO: Bad Smell Code is repiitive and unreeadible
@@ -134,35 +181,37 @@ class Image:
         (X_top_left_bottom_left, Y_top_left_bottom_left) = points[1][2]
         (X_top_right_bottom_right, Y_top_right_bottom_right) = points[1][3]
 
-        # draw the midpoints on the image
-        #TODO: Bad Smell, repeditive code, Should be created into its own object
-        #TODO: Replace numbers 5 and -1 with something more meaningfull
-        cv2.circle(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)), 5, RED, -1)
-        cv2.circle(orig, (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)), 5, RED, -1)
-        cv2.circle(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)), 5, RED, -1)
-        cv2.circle(orig, (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)), 5, RED, -1)
-
         # draw lines between the midpoints
-        cv2.line(orig, (int(X_top_left_top_right), int(Y_top_left_top_right)),
-                 (int(X_bottom_left_bottom_right), int(Y_bottom_left_bottom_right)),
-                 PINK, 2)
-        cv2.line(orig, (int(X_top_left_bottom_left), int(Y_top_left_bottom_left)),
-                 (int(X_top_right_bottom_right), int(Y_top_right_bottom_right)),
-                 PINK, 2)
+        self.draw_centre_line(X_bottom_left_bottom_right, X_top_left_bottom_left, X_top_left_top_right,
+                              X_top_right_bottom_right, Y_bottom_left_bottom_right, Y_top_left_bottom_left,
+                              Y_top_left_top_right, Y_top_right_bottom_right, orig)
 
-        # Drawing the objects according to their size on the image
-        cv2.putText(orig, "{:.1f}in".format(distX / pixelsPerUnit),
-                    (int(X_top_left_top_right - 15), int(Y_top_left_top_right - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
-                    WHITE, 2)
-        cv2.putText(orig, "{:.1f}in".format(distY / pixelsPerUnit),
-                    (int(X_top_right_bottom_right + 10), int(Y_top_right_bottom_right)), cv2.FONT_HERSHEY_SIMPLEX, 0.65,
-                    WHITE, 2)
+        self.print_dimensions_on_object(X_top_left_top_right, X_top_right_bottom_right, Y_top_left_top_right,
+                                        Y_top_right_bottom_right, distX, distY, orig, pixelsPerUnit)
 
         # show the generated image
         cv2.imshow("Image", orig)
         cv2.waitKey(0)
 
+class Measurment():
+    #Contor of intrest
+    #Dimensions x,y
+    #Location
+    #Image with box
+    
+    def __init__(self):
+        self.placeholder="Placeholder"
+    
+    def rnd(self):
+        print(self)
+
 #Test of the code
 test_image=Image("Image_refrence_4.jpg")
 test_image.main()
+
+cntores, image = test_image.extract_contors_and_import_image("Image_refrence_4.jpg")
+cnt1=cntores[0]
+cnt2=cntores[1]
+cnt3=cntores[2]
+
 
